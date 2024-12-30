6fd150e0d6e262101aa113a5d4e29c3072a1a268
workmusicalflow <studioabidjanpro1@gmail.com>
2024-12-30 14:12:31 +0000

    refactor: mise à jour de index.html et configuration tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,23 @@
 module.exports = {
   content: ["./src/**/*.{html,js,php}"],
   theme: {
-    extend: {},
+    extend: {
+      keyframes: {
+        "fade-in-up": {
+          "0%": {
+            opacity: "0",
+            transform: "translateY(20px)",
+          },
+          "100%": {
+            opacity: "1",
+            transform: "translateY(0)",
+          },
+        },
+      },
+      animation: {
+        "fade-in": "fade-in-up 0.5s ease-out forwards",
+      },
+    },
   },
   plugins: [],
 };
